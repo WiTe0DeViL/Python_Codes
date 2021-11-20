55be44273fc740c70b88b50ea748b23ef3d2c113
Man_Of_Darkness <72191625+WiTe0DeViL@users.noreply.github.com>
2021-11-21 00:15:06 +0530

    Duplicate

--- Duplicate_remover.py
+++ Duplicate_remover.py
@@ -1,9 +1,9 @@
 a = list(input("Enter the string to remove Duplicates : \n"))
 
-res = []
+result = []
 for i in a:
-    if i not in res:
-        res.append(i)
+    if i not in result:
+        result.append(i)
 
-for x in res:
+for x in result:
     print(x, end="")
