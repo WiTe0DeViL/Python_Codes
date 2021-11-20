4d7d3f152c8f6f6d118c9ba4abdb8a920b15bcc9
Man_Of_Darkness <72191625+WiTe0DeViL@users.noreply.github.com>
2021-11-20 11:09:16 +0530

    random_Matrix

--- matrix.py
+++ matrix.py
@@ -1,3 +1,5 @@
+from random import randint as ri
+
 r = int(input("Enter the rows : "))
 c = int(input("Enter the columns : "))
 
@@ -14,6 +16,32 @@ for i in range(r):
 
 # displaying matrix
 for i in range(r):
+    st = "| "
     for j in range(c):
-        print(matrix[i][j], end=" ")
-    print()
+        if matrix[i][j] < 10:
+            st += str(matrix[i][j]) + "  | "
+        else:
+            st += str(matrix[i][j]) + " | "
+    print(st)
+
+print("~~~~~~~~~Random Matrix~~~~~~~~~~~~")
+print()
+
+Random_matrix = []
+
+# Random matrix
+for i in range(r):
+    b = []
+    for j in range(c):
+        b.append(ri(0, 99))
+    Random_matrix.append(b)
+
+# displaying Random_matrix
+for i in range(r):
+    st = "| "
+    for j in range(c):
+        if Random_matrix[i][j] < 10:
+            st += str(Random_matrix[i][j]) + "  | "
+        else:
+            st += str(Random_matrix[i][j]) + " | "
+    print(st)
