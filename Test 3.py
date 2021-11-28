a2bbedddf2adba3c96eb12b987b0b43390bbe687
Suren Kumar <surentamarai222@gmail.com>
2021-11-27 20:42:12 -0800

    TimeDate

--- Test 3.py
+++ Test 3.py
@@ -1,5 +1,21 @@
 import datetime as t
+from time import sleep as s 
+import sys
+
+
+def typeText(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        s(0.10)
 
 a = t.datetime.now()
 
-print(a.strftime("%Y-%m-%d %H-%M-%S"))
+typeText("Time Enna theriyuma seri iru na pathu Solra\n")
+
+s(3)
+typeText("Time oda Datum sollava...... iru\n")
+
+s(3)
+
+typeText(a.strftime("%Y-%m-%d %H-%M-%S"))
